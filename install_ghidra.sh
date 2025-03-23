ba3a8594ce121bf8cd0c3f2066c2c75fc3c5e69b
ArbitraryPrism <65346600+ArbitraryPrism@users.noreply.github.com>
2025-03-23 09:58:04 +0000

    Update install_ghidra.sh

--- install_ghidra.sh
+++ install_ghidra.sh
@@ -10,12 +10,15 @@ printf "\n\n[+] Getting Ghidra...\n\n"
 Latest_Ghidra_Version=$(wget https://github.com/NationalSecurityAgency/ghidra/releases/latest 2>&1 | grep Location: | grep -Eo 'https://[^ >]+' | head -1 | awk -F/  '{print $NF}')
 Latest_Ghidra=$(wget -qO- https://github.com/NationalSecurityAgency/ghidra/releases/expanded_assets/$Latest_Ghidra_Version 2>&1 | grep -Eo 'ghidra_.*zip')
 wget -O ~/Downloads/ghidra.zip https://github.com/NationalSecurityAgency/ghidra/releases/download/$Latest_Ghidra_Version/$Latest_Ghidra
-FolderName=$(unzip -qql ~/Downloads/ghidra.zip | head -n1 | awk '{print $4}')
-unzip ~/Downloads/ghidra.zip -d ~/
+mkdir ~/Applications
+# FolderName=$(unzip -qql ~/Downloads/ghidra.zip | head -n1 | awk '{print $4}')
+FolderName=Ghidra
+unzip ~/Downloads/ghidra.zip -d ~/Applications
+mv ~/Applications/$(unzip -qql ~/Downloads/ghidra.zip | head -n1 | awk '{print $4}') Ghidra
 
 # Update the image...
 printf "[+] Getting the icon image...\n\n"
-wget -O ~/${FolderName}support/ghidra.png https://github.com/ArbitraryPrism/GhidraSetup/raw/main/ghidra.png
+wget -O ~/Applications/${FolderName}/support/ghidra.png https://github.com/ArbitraryPrism/GhidraSetup/raw/main/ghidra.png
 
 # Set up the desktop file...
 printf "\n\n[+] Creating desktop file...\n\n"
@@ -25,9 +28,9 @@ chmod +x ~/.local/share/applications/ghidra.desktop
 
 # Now update the .desktop file...
 printf "\n\n[+] Updating desktop file...\n\n"
-ExecutePath=~/${FolderName}ghidraRun
-IconPath=~/${FolderName}support/ghidra.png
-GhidraPath=~/${FolderName}
+ExecutePath=~/Applications/${FolderName}/ghidraRun
+IconPath=~/Applications/${FolderName}/support/ghidra.png
+GhidraPath=~/Applications/${FolderName}/
 sed -i "s@ExecPath@$ExecutePath@g" ~/.local/share/applications/ghidra.desktop
 sed -i "s@IconPath@$IconPath@g" ~/.local/share/applications/ghidra.desktop
 sed -i "s@GhidraPath@$GhidraPath@g" ~/.local/share/applications/ghidra.desktop
